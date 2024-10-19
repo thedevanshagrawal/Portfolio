139d4b7a19be2e2eb43b267c6a5f1da0974c0105
thedevanshagrawal <thedevanshagrawal@gmail.com>
2024-10-19 17:45:43 +0530

    new file:   next/.eslintrc.json 	new file:   next/.gitignore 	new file:   next/README.md 	new file:   next/next.config.mjs 	new file:   next/package-lock.json 	new file:   next/package.json 	new file:   next/postcss.config.mjs 	new file:   next/public/bg.webp 	new file:   next/public/c-.png 	new file:   next/public/coderbg.webp 	new file:   next/public/computer.jpg 	new file:   next/public/css.png 	new file:   next/public/developer.png 	new file:   next/public/github.png 	new file:   next/public/html-5.png 	new file:   next/public/instagram.png 	new file:   next/public/js-file.png 	new file:   next/public/linkedin.png 	new file:   next/public/logo.svg 	new file:   next/public/node.png 	new file:   next/public/python.png 	new file:   next/public/react.png 	new file:   next/src/app/ProjectSection/ProjectSection.tsx 	new file:   next/src/app/ProjectSection/page.tsx 	new file:   next/src/app/favicon.ico 	new file:   next/src/app/fonts/GeistMonoVF.woff 	new file:   next/src/app/fonts/GeistVF.woff 	new file:   next/src/app/globals.css 	new file:   next/src/app/layout.tsx 	new file:   next/src/app/page.tsx 	new file:   next/src/components/HomeSection.tsx 	new file:   next/src/components/Navbar.tsx 	new file:   next/src/components/ui/3d-card.tsx 	new file:   next/src/components/ui/navbar-menu.tsx 	new file:   next/src/lib/utils.ts 	new file:   next/tailwind.config.ts 	new file:   next/tsconfig.json

---
+++ next/src/app/page.tsx
@@ -0,0 +1,12 @@
+import HomeSection from "@/components/HomeSection";
+import Navbar from "@/components/Navbar";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <>
+   <Navbar/>
+   <HomeSection/>
+   </>
+  );
+}
